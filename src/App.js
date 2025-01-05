611f838043f0b5142f146e914449bbe8b6c89740
Shreelaxmi-M <shreelaxmimalwad24@gmail.com>
2025-01-05 14:31:50 +0530

    routing issue fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useNavigate } from 'react-router';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Home from './components/Home';
@@ -6,26 +6,33 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
-  const navigate = useNavigate();
+
   useEffect(() => {
-    //check if email exists in localstorage
+    // Check if email exists in localStorage and set the authentication state
     const email = localStorage.getItem("email");
-    setIsAuthenticated(email);
-  }, [navigate])
+    setIsAuthenticated(email);  // This will be null or the email value
+  }, []);
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Navigate to={"/login"} />} />
-        <Route
-          path="/login"
-          element={isAuthenticated ? <Navigate to='/home' /> : <Login />}
-        />
-        <Route
-          path="/home"
-          element={isAuthenticated ? <Home /> : <Navigate to='/login' />}
-        />
+      <BrowserRouter>
+        <Routes>
+          {/* Redirect "/" to the "/login" page */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          
+          {/* Route for login */}
+          <Route
+            path="/login"
+            element={isAuthenticated ? <Navigate to="/home" /> : <Login />}
+          />
 
-      </Routes>
+          {/* Route for home */}
+          <Route
+            path="/home"
+            element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
